63f7ff20cb040a02909ab451372ee408def2de12
santhoshraddp <E1>
2018-03-09 10:36:56 +0530

    edygvbuincommit

---
+++ ee/execute.bat
@@ -0,0 +1,15 @@
+cd C:\Users\SA389141\Desktop\BasicExpressnode\downloads\SI273181
+git
+git init
+git config --global user.name "santhoshraddp"
+git config --global user.email "E1"
+git config --global credential.helper wincred
+git add ee
+git commit -m "  edygvbuincommit"
+git remote -v
+git remote set-url origin https://santhoshraddp:Santhosh123@github.com/santhoshraddp/asdf.git
+git remote add origin https://github.com/santhoshraddp/asdf.git
+git push -u origin   master
+echo Press Enter...
+pause
+
